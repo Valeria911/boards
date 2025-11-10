5a2dad2b8c7cae4b4268f909b974357973f2b78b
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2025-11-10 13:15:48 -0300

    Logica para recuperar contraseña

---
+++ js/recuperar.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form-recuperar");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value.trim();
+    const nueva = document.getElementById("nuevaPassword").value.trim();
+    const confirmar = document.getElementById("confirmarPassword").value.trim();
+    if (!validarEmail("email") || !validarPassword("nuevaPassword", "confirmarPassword")) {
+      return; 
+    }
+
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    const index = usuarios.findIndex(u => u.email === email);
+    if (index === -1) {
+      alert("No existe una cuenta asociada a ese correo. Debes registrarte primero.");
+      window.location.href = "registro.html";;
+    }
+
+    usuarios[index].password = nueva;
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+    alert("Contraseña actualizada correctamente. Ahora puedes iniciar sesión");
+    window.location.href = "login.html";
+  });
+});
